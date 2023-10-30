c16b810d7574cd00bfe1352e35219ca294465a59
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-10-30 16:21:37 +0200

    adds draft slider

---
+++ src/js/swiper.js
@@ -0,0 +1,29 @@
+import Swiper, { Autoplay } from 'swiper';
+
+const mySwiper = new Swiper('.swiper', {
+  modules: [Autoplay],
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  speed: 2000,
+  centeredSlides: true,
+  breakpointsBase: 'container',
+  breakpoints: {
+    360: {
+      slidesPerView: 1.2,
+      spaceBetween: 7,
+    },
+
+    834: {
+      slidesPerView: 1.5,
+      spaceBetween: 18,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 18,
+    },
+  },
+});
