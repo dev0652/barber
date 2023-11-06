b0706a6f1ef57896e90b345c2fa595598f2cb28a
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-11-06 12:13:14 +0200

    notifix. site developers

--- src/js/send-message.js
+++ src/js/send-message.js
@@ -1,30 +1,55 @@
 import emailjs from '@emailjs/browser';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 (function () {
   emailjs.init('2np3E1HG1gcX8Tapf');
 })();
 
-const contactForm = document
-  .getElementById('contact-form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
+// const contactForm = document
+//   .getElementById('contact-form')
+//   .addEventListener('submit', function (event) {
+//     event.preventDefault();
 
-    emailjs
-      .sendForm(
-        'service_02rwk5r',
-        'template_p886z4c',
-        '#contact-form',
-        '2np3E1HG1gcX8Tapf'
-      )
-      .then(
-        function (response) {
-          alert('SUCCESS!');
-        },
-        function (error) {
-          alert('FAILED...');
-        }
-      );
-  });
+//     emailjs
+//       .sendForm(
+//         'service_02rwk5r',
+//         'template_p886z4c',
+//         '#contact-form',
+//         '2np3E1HG1gcX8Tapf'
+//       )
+//       .then(
+//         function (response) {
+//           alert('SUCCESS!');
+//         },
+//         function (error) {
+//           alert('FAILED...');
+//         }
+//       );
+//   });
+
+const allForm = document.querySelectorAll('.contact_form');
+
+// allForm.forEach(element => {
+//   element.addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     emailjs
+//       .sendForm(
+//         'service_02rwk5r',
+//         'template_p886z4c',
+//         '.contact_form',
+//         '2np3E1HG1gcX8Tapf'
+//       )
+//       .then(
+//         function (response) {
+//           alert('SUCCESS!');
+//         },
+//         function (error) {
+//           alert('FAILED...');
+//         }
+//       );
+//   });
+// });
 
 const mailForm = document
   .getElementById('email-form')
@@ -40,10 +65,10 @@ const mailForm = document
       )
       .then(
         function (response) {
-          alert('SUCCESS!');
+          Notify.success('Успішно надіслано');
         },
         function (error) {
-          alert('FAILED...');
+          Notify.failure('Щось пішло не так!');
         }
       );
   });
